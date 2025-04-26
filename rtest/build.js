595bd6e3d8623b06e8fa26773fb9b25c9b9a2ce7
farhan <farhanalirazaazeemi@gmail.com>
2025-04-26 19:18:57 +0500

    deleted things

--- rtest/build.js
+++
@@ -1,6 +0,0 @@
-import { render } from '@react-email/render';
-
-// We can't dynamically import a .tsx file in Node.js without transpilation
-// This file should be run with 'pnpx tsx build.tsx' instead
-console.log('Please use: pnpx tsx build.tsx');
-console.log('JSX syntax is not supported in plain .js files without transpilation.');
