4539892fb14d240b33a9049dee91173d0722f05b
farhan <farhanalirazaazeemi@gmail.com>
2025-04-26 19:19:33 +0500

    Remove unused email rendering script and generated HTML file

--- build.js
+++
@@ -1,8 +0,0 @@
-import AWSVerifyEmail from './emails/my-email.tsx';
-import { render } from '@react-email/render';
-
-const html = render(AWSVerifyEmail({ verificationCode: '123456' }), {
-    pretty: true,
-});
-
-console.log(html);
